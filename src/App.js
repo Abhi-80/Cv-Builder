ef3f65732bc331cb2c62cc0d746d8298b4787508
अभिषेक कुमार ठाकुर <131022199+Abhi-80@users.noreply.github.com>
2024-02-20 10:01:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-
-import React from "react";
-import "./App.css";
-import Banner from "./Components/Banner";
-import Nav from "./Components/Nav";
-import Row from "./Components/Row";
-import request from "./request";
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      {/* navbar */}
-      <Nav />
-      {/* Banner */}
-      <Banner />
-
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={request.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="TRENDING NOW" fetchUrl={request.fetchTrending} />
-      <Row title="TOP RATED" fetchUrl={request.fetchTopRated} />
-      <Row title="ACTION MOVIES" fetchUrl={request.fetchActionMovies} />
-      <Row title="COMEDY MOVIES" fetchUrl={request.fetchComedyMovies} />
-      <Row title="HORROR MOVIES" fetchUrl={request.fetchHorrorMovies} />
-      <Row title="ROMANCE MOVIES" fetchUrl={request.fetchRomanceMovies} />
-      <Row title="DOCUMENTARIES " fetchUrl={request.fetchDocumentaries} />
+    <div className = "App">
+      <Header/>
     </div>
   );
 }
